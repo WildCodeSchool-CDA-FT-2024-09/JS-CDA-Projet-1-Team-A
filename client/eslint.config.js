2d11fcbe55c289da07cc9eb6ba4198ef13ff3c70
Gadln <helene.gadessaud@icloud.com>
2024-10-16 12:09:23 +0200

    client/setup: add eslint console config and script lint and format

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -23,6 +23,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      "no-console":["error", { allow: ["error", "info", "warm"]}]
     },
   },
 )
