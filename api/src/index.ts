fc87472bafad24037d50b25d63244d527070a20b
Thomas Allen <thomasedwardallen@gmail.com>
2024-10-17 14:29:15 +0200

    setup/api: install dotenv, setup env variable for port in api init

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,13 @@
 import "reflect-metadata";
+import * as dotenv from "dotenv";
 import { buildSchema } from "type-graphql";
-import { appDataSource } from "./db/data.source";
+import { appDataSource } from "./db/db-client";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import TestResolver from "./test/test.resolver";
+
+dotenv.config();
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 4000;
 
 (async () => {
   await appDataSource.initialize();
@@ -16,7 +20,7 @@ import TestResolver from "./test/test.resolver";
   });
 
   const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+    listen: { port: PORT },
   });
 
   console.info(`🚀  Server ready at: ${url}`);
