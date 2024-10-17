8dc87b3b36525970a28a737ea60e7911c4da4c9f
julien44830 <deniaudjulien0@gmail.com>
2024-10-17 11:54:07 +0200

    setup/api problème de signature decorateur & tsconfig

---
+++ api/src/test.entities/test.entities.ts
@@ -0,0 +1,15 @@
+// import "reflect-metadata";
+// import { BaseEntity, Entity, Column, PrimaryColumn } from "typeorm";
+// import { Field, ObjectType } from "type-graphql";
+
+// @ObjectType()
+// @Entity()
+// export class Test extends BaseEntity {
+//   @Field()
+//   @PrimaryColumn()
+//   id: number;
+
+//   @Field()
+//   @Column()
+//   name: string;
+// }
