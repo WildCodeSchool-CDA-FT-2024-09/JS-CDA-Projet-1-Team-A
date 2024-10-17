cc84c8368807e5c034317ec44289bf46e169f346
pierre delarocque <delarocque.pierre@gmail.com>
2024-10-17 15:23:47 +0200

    client/ testing husky

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+          <h4>kikouuu</h4>
         </a>
       </div>
       <h1>Vite + React</h1>
