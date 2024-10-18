cf7327608a0e44490d9b4c9b4745f661f8c05b0f
Thomas Allen <thomasedwardallen@gmail.com>
2024-10-18 10:48:05 +0200

    client/components: add placeholder folders for React structure - components, contexts, pages. Clean App, add basic GodList component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import GodsList from "./components/godsList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-          <h4>kikouuu</h4>
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <h1>I AM A HEADING</h1>
+      <h1 className="text-2xl font-bold">Gods Arena</h1>
+      <GodsList />
+      <footer className="mt-12">
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          (Icons 🙏{" "}
+          <a className="hover:underline" href="https://www.flaticon.com">
+            FlatIcon
+          </a>
+          )
         </p>
-      </div>
-      <p className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
-        There was an ESLint error here and now there is not. This paragraph is
-        styled with Tailwind.
-      </p>
+      </footer>
     </>
   );
 }
