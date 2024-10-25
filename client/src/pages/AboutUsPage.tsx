5db7f5afb1447656a1de43b15adafe4726e431b9
Gadln <helene.gadessaud@icloud.com>
2024-10-25 11:07:20 +0200

    client/aboutus html tags fixed

--- client/src/pages/AboutUsPage.tsx
+++ client/src/pages/AboutUsPage.tsx
@@ -8,8 +8,8 @@ function AboutUsPage() {
           Nous connaitre
         </h1>
 
-        <div className="text-blue-v">
-          <div className="mb-4">
+        <section className="text-blue-v">
+          <article className="mb-4">
             <h2 className="mb-1 font-bold">
               Bienvenue dans l’arène des dieux !
             </h2>
@@ -20,8 +20,8 @@ function AboutUsPage() {
               incarne un dieu grec, avec des pouvoirs et des capacités uniques,
               prêt à relever des défis épiques.
             </p>
-          </div>
-          <div className="mb-4">
+          </article>
+          <article className="mb-4">
             <h2 className="mb-1 font-bold">L'histoire</h2>
             <p className="text-justify">
               Les dieux ont décidé d'organiser une série d'épreuves pour
@@ -32,17 +32,17 @@ function AboutUsPage() {
               stratégie, le courage et la force des dieux seront tes plus grands
               atouts. Que les dieux te soient favorables !
             </p>
-          </div>
-        </div>
+          </article>
+        </section>
 
-        <div className="mb-4 font-bold text-blue-v">
+        <section className="mb-4 font-bold text-blue-v">
           <h2 className="mb-2 mt-4 text-xl">L'équipe Reactaulos</h2>
           <p>Hélène</p>
           <p>Adrien</p>
           <p>Pierre</p>
           <p>Tom</p>
           <p>Julien</p>
-        </div>
+        </section>
 
         <a
           href="https://github.com/WildCodeSchool-CDA-FT-2024-09/JS-CDA-Projet-1-Team-A"
--- client/src/pages/RulesPage.tsx
+++ client/src/pages/RulesPage.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function RulesPage() {
   return (
     <main>
-      <div className="mt-38 fixed left-1/2 top-1/2 mx-auto mb-8 max-h-[55vh] w-[70%] -translate-x-1/2 -translate-y-1/2 transform overflow-y-auto rounded-xl bg-yellow-p bg-opacity-85 p-8 text-justify text-blue-v md:w-[40%] lg:w-1/3">
+      <section className="mt-38 fixed left-1/2 top-1/2 mx-auto mb-8 max-h-[55vh] w-[70%] -translate-x-1/2 -translate-y-1/2 transform overflow-y-auto rounded-xl bg-yellow-p bg-opacity-85 p-8 text-justify text-blue-v md:w-[40%] lg:w-1/3">
         <h1 className="mb-4 text-center text-2xl font-bold">Règles du jeu</h1>
         <p>
           Vous créez votre champion en choisissant un nom et un avatar puis
@@ -13,7 +13,7 @@ function RulesPage() {
           de chaque champion et les conditions météorologiques du jour pourront
           peut être influencer sur les résultats ...
         </p>
-      </div>
+      </section>
       <Link
         to="/"
         aria-label="Retourner à la page d'accueil"
