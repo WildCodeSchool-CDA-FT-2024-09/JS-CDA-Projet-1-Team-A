db38d65e7bce3516d11e9afc564cf8c06173abd5
julien44830 <deniaudjulien0@gmail.com>
2024-10-30 17:09:19 +0100

    client:feature: modale dynamique

--- client/src/components/trial/ModaleResultTrial.tsx
+++ client/src/components/trial/ModaleResultTrial.tsx
@@ -12,12 +12,33 @@ export default function ModaleResultTrial({
       <section
         className={
           result
-            ? "mt-[4rem] flex h-[80vw] max-h-[700px] w-[65vw] min-w-[280px] max-w-[700px] flex-col items-center justify-center rounded-xl bg-blue-fd bg-opacity-85 md:justify-evenly"
+            ? "relative flex h-[62vh] max-h-[62vh] w-[65vw] min-w-[280px] max-w-[700px] flex-col items-center justify-between rounded-xl bg-blue-fd bg-opacity-85 py-10"
             : "hidden"
         }
       >
+        <img
+          src="./img/competitors/avatars/avatar.png"
+          alt="avatar du joueur"
+          className={`w-44 rounded-xl ${resultTrial.status === "VICTOIRE" ? "" : "saturate-0"}`}
+        />
+
         {/*affiche la modale quand le state result passe a true*/}
-        <h2>ici le resulta du combat {resultTrial.status}</h2>
+        {resultTrial.status === "VICTOIRE" ? (
+          <>
+            <img
+              src="/img/item/cup.png"
+              alt="coupe de la victoire"
+              className="absolute mt-36 w-48"
+            />
+            <img
+              src="/img/item/confetti.png"
+              alt="confettis de la victoire"
+              className="absolute mt-0 w-fit"
+            />
+          </>
+        ) : null}
+
+        <h2 className="mb-28 text-3xl font-bold">{resultTrial.status}</h2>
       </section>
     </div>
   );
--- client/src/pages/StartTrialPage.tsx
+++ client/src/pages/StartTrialPage.tsx
@@ -3,12 +3,12 @@ import ModaleResultTrial from "../components/trial/ModaleResultTrial";
 
 // en attente du recupérer les information du résulta d'épreuve
 const resultTrial: { status: string }[] = [
-  { status: "Victoire" },
-  { status: "Défaite" },
+  { status: "VICTOIRE" },
+  { status: "DEFAITE" },
 ];
 
 function StartTrialPage() {
-  const [result, setResult] = useState(true);
+  const [result, setResult] = useState(false);
 
   return (
     <section className="fixed inset-0 h-screen w-full bg-black/50 backdrop-blur-sm">
@@ -17,10 +17,12 @@ function StartTrialPage() {
         <h1 className="text-4xl text-white">Votre Contenu</h1>
       </div>
       {/* modale qui change en fonction du resulta pas encore fonctionnel */}
-      <ModaleResultTrial result={result} resultTrial={resultTrial[1]} />,
+      <ModaleResultTrial result={result} resultTrial={resultTrial[0]} />
+
       {/* bouton pour afficher le resulta de l'épreuve */}
+
       <button
-        className="btn-primary fixed bottom-[100px] left-1/2 mx-0 min-w-[220px] max-w-[230px] -translate-x-1/2 transform"
+        className={`btn-primary fixed left-1/2 mx-0 min-w-[220px] max-w-[230px] -translate-x-1/2 transform ${!result ? "bottom-[100px]" : "bottom-[200px]"}`}
         onClick={() => setResult(!result)}
       >
         Démarrer l'épreuve
