3fabc2d108ac99ee07e2bc8cceaec76612ae79e5
Adri-bis <adrien.ma.16@hotmail.fr>
2024-10-30 10:35:55 +0100

    client/fix: commit before merge dev, change the name page creat to create

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 import client from "./services/connexion.ts";
-import CreateCharacterPage from "./pages/CreatCharacterPage.tsx";
+import CreateCharacterPage from "./pages/CreateCharacterPage.tsx";
 
 const router = createBrowserRouter([
   {
