380ac924b12d41bb94ab401a799a0f1f4063e647
Thomas Allen <thomasedwardallen@gmail.com>
2024-10-31 10:15:16 +0100

    client/statistics: update statistics page to use combat stats query and display basic history of combats

--- client/src/pages/StatisticsPage.tsx
+++ client/src/pages/StatisticsPage.tsx
@@ -1,18 +1,38 @@
 import { Link } from "react-router-dom";
+import { useGetCombatStatsQuery } from "../generated/graphql-types";
+
+function formatCombatDate(date: string) {
+  return new Date(date).toLocaleDateString("fr-FR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
 
 function StatisticsPage() {
+  const { data, loading, error } = useGetCombatStatsQuery();
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+  if (!data) return <p>No data</p>;
+  const { combats } = data;
   return (
     <main>
       <div className="mt-38 text-justif fixed left-1/2 top-1/2 mx-auto mb-8 max-h-[55vh] w-[70%] -translate-x-1/2 -translate-y-1/2 transform overflow-y-auto rounded-xl bg-yellow-p bg-opacity-85 p-6 md:w-[40%] lg:w-1/3">
         <h1 className="mb-4 text-center text-2xl font-bold text-blue-v">
-          Statistiques
+          L'histoire des combats
         </h1>
-
-        <section className="text-blue-v">
-          <article className="mb-4">
-            <h2 className="mb-1 font-bold">Achilles vs. Hector</h2>
-            <p className="text-justify">TODO - combat overview</p>
-          </article>
+        <section>
+          <ul>
+            {combats.map((combat) => (
+              <li key={combat.id} className="mb-4 text-blue-v">
+                <h2 className="mb-1 font-bold text-stone-50">
+                  {`${combat.player?.name} (${combat.playerGod.name})  vs. ${combat.opponent?.name} (${combat.opponentGod.name})`}
+                </h2>
+                <p>Le {formatCombatDate(combat.createdAt)}</p>
+                <p>Épreuve de {combat.trial.name}</p>
+              </li>
+            ))}
+          </ul>
         </section>
       </div>
       <Link
