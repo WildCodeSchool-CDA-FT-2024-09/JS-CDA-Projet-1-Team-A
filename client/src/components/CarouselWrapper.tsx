6699aef6d037661c2ac2f0e7082907975adc27bc
Adri-bis <adrien.ma.16@hotmail.fr>
2024-11-05 16:03:26 +0100

    client/feature : change and refacto all the carousel in only one

--- client/src/components/CarouselWrapper.tsx
+++ client/src/components/CarouselWrapper.tsx
@@ -1,15 +1,12 @@
 import { useState, useEffect } from "react";
-import AvatarCarouselDesktop from "./competitor/AvatarCarouselDesktop";
-import AvatarCarouselMobile from "./competitor/AvatarCarouselMobile";
+import AvatarCarousel from "./competitor/AvatarCarousel";
 
-interface CarouselWrapperProps {
-  imageUrls: { url: string }[];
-}
-const AvatarCarouselWrapper = ({ imageUrls }: CarouselWrapperProps) => {
+const AvatarCarouselWrapper = ({ imageUrls }) => {
   const [isMobile, setIsMobile] = useState(false);
-  // const [currentIndex, setCurrentIndex] = useState(3);
-  // const [startX, setStartX] = useState<number | null>(null);
-  // const [isDragging, setIsDragging] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(3);
+  const [startX, setStartX] = useState<number | null>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const totalImages = imageUrls.length;
 
   const handleResize = () => {
     setIsMobile(window.innerWidth < 768);
@@ -21,15 +18,63 @@ const AvatarCarouselWrapper = ({ imageUrls }: CarouselWrapperProps) => {
 
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+  const handleDragMove = (diff: number) => {
+    const numberPx = isMobile ? 25 : 50;
+    if (diff > numberPx) {
+      handleNav(1);
+      resetDrag();
+    } else if (diff < -numberPx) {
+      handleNav(-1);
+      resetDrag();
+    }
+  };
+
+  const resetDrag = () => {
+    setStartX(null);
+    setIsDragging(false);
+  };
+
+  const handleTouchStart = (e: React.TouchEvent) => {
+    setStartX(e.touches[0].clientX);
+    setIsDragging(true);
+  };
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    setStartX(e.clientX);
+    setIsDragging(true);
+  };
+
+  const handleTouchMove = (e: React.TouchEvent) => {
+    if (!isDragging || startX === null) return;
+    const touchEndX = e.touches[0].clientX;
+    handleDragMove(startX - touchEndX);
+  };
+
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (!isDragging || startX === null) return;
+    const mouseEndX = e.clientX;
+    handleDragMove(startX - mouseEndX);
+  };
+
+  const handleNav = (direction: number) => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex + direction + totalImages) % totalImages
+    );
+  };
 
   return (
-    <>
-      {isMobile ? (
-        <AvatarCarouselMobile imageUrls={imageUrls} />
-      ) : (
-        <AvatarCarouselDesktop imageUrls={imageUrls} />
-      )}
-    </>
+    <AvatarCarousel
+      imageUrls={imageUrls}
+      handleTouchStart={handleTouchStart}
+      handleTouchMove={handleTouchMove}
+      handleTouchEnd={resetDrag}
+      handleMouseDown={handleMouseDown}
+      isDragging={isDragging}
+      handleMouseMove={handleMouseMove}
+      handleMouseUp={resetDrag}
+      currentIndex={currentIndex}
+      mobile={isMobile}
+    />
   );
 };
 
