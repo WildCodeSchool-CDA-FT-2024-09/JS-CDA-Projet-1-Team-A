57c2f8f0e57dae7c5dfe2f6283247bdea3460b30
julien44830 <deniaudjulien0@gmail.com>
2024-11-08 11:29:50 +0100

    remove <p> to ModailResultDetail

--- client/src/components/trial/ModaleResultDetail.tsx
+++ client/src/components/trial/ModaleResultDetail.tsx
@@ -17,7 +17,6 @@ const ModaleResultDetail: React.FC<ModaleResultDetailProps> = ({
       <section className="relative flex h-[62vh] max-h-[62vh] w-[65vw] min-w-[280px] max-w-[700px] flex-col items-center justify-between rounded-xl bg-blue-fd bg-opacity-85 py-10">
         <p>Résultat : {result.status}</p>
         <p>Résultat : {textTrial}</p>
-        <p>Résultat : {textTrial}</p>
         <p>Message : {result.valueBtn}</p>
       </section>
     </div>
